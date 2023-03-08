f158459897519e1935776ad66086537f0ffa4392
Johannes Teichrieb <isrvoid@gmail.com>
2023-03-08 13:19:30 +0100

    Move tree allocator into own repo
    It won't be needed here.

--- build.zig
+++ build.zig
@@ -8,30 +8,12 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    trpc.addCSourceFile("src/tree_allocator.c", &.{});
+    trpc.addCSourceFile("src/temp_dummy.c", &.{});
     trpc.addIncludePath("src");
     trpc.linkLibC();
 
-    const exe = b.addExecutable(.{
-        .name = "benchmark",
-        .root_source_file = .{ .path = "src/benchmark.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.linkLibrary(trpc);
-    exe.addIncludePath("src");
-    exe.install();
-
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     const exe_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/test_runner.zig" },
         .target = target,
         .optimize = optimize,
     });
--- src/main.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-pub fn main() void {
-}
-
-test "allocator" {
-    @import("test_allocator.zig").enforceInclude();
-}
